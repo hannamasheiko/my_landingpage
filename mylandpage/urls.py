0cf3354d9f30a81eabbe26a484fa2e90a29a43eb
Hanna <hannamasheiko@gmail.com>
2017-06-02 22:03:41 +0300

    bugfix

--- mylandpage/urls.py
+++ mylandpage/urls.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
      url(r'^$', 'landinform.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls))
 
 ]
